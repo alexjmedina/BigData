dcefcfd8ec9d5dfec0b198e358b47cb4cd8947c4
Alexander Medina <alexjmedina@users.noreply.github.com>
2023-07-07 20:36:02 -0400

    MRJob example

---
+++ CompteEvals.py
@@ -0,0 +1,20 @@
+""" Programme pour compter les nombres de evaluations du fichier u.data """
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class CompteEvals(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_evals,
+                   reducer=self.reducer_compte_evals)       ]
+
+    def mapper_get_evals(self, _, line):
+        colonnes = line.split("\t")
+        evals = colonnes[2]
+        yield evals, 1
+
+    def reducer_compte_evals(self, eval, uns):
+        yield eval, sum(uns)
+
+if __name__ == '__main__':
+    CompteEvals.run()
