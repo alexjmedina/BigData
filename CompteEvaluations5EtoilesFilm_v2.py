c82136cd8665bc4f09457c27b5746330961f6ce1
Alexander Medina <alexjmedina@users.noreply.github.com>
2023-08-09 20:32:57 -0400

    Updating

---
+++ CompteEvaluations5EtoilesFilm_v2.py
@@ -0,0 +1,23 @@
+""" Programme pour compter les mots d'un texte et afficher pour chaque mot le nombre d'occurence"""
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class CompteEvaluationsFilm(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_evaluations,
+                   reducer=self.reducer_compte_evaluations)       ]
+
+    def mapper_get_evaluations(self, _, line):
+         
+        colonnes = line.split("\t")
+        eval = colonnes[2]
+        if eval == "5":
+            film = colonnes[1]
+            yield film, 1
+
+    def reducer_compte_evaluations(self, eval, uns):
+        yield eval, sum(uns)
+
+if __name__ == '__main__':
+    CompteEvaluationsFilm.run()
