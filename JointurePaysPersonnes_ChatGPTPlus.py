7cdcfaaf41d7fbee28de8b4188a01200ecae729f
Alexander Medina <alexjmedina@users.noreply.github.com>
2023-08-25 15:22:40 -0400

    Updating

--- JointurePaysPersonnes_ChatGPTPlus.py
+++ JointurePaysPersonnes_ChatGPTPlus.py
@@ -2,38 +2,33 @@ from mrjob.job import MRJob
 from mrjob.step import MRStep
 
 class JointurePaysPersonnes(MRJob):
+
     def steps(self):
         return [
-            MRStep(mapper=self.mapper_get_pays,
-                   reducer=self.reducer_compte_pays)       ]
+            MRStep(mapper=self.map_records, reducer=self.join_by_country_code)
+        ]
 
-    def mapper_get_pays(self, _, line):
+    def map_records(self, _, line):
         colonnes = line.split("|")
         
-        # Check if record has 2 columns (country data)
+        # Country data
         if len(colonnes) == 2:
             pays, pays_code = colonnes
             yield pays_code, ('country', pays)
 
-        # Check if record has 3 columns (person data)
+        # Person data
         elif len(colonnes) == 3:
             prenom, nom, pays_code = colonnes
             yield pays_code, ('person', prenom, nom)
-                     
 
-    def reducer_compte_pays(self, pays_code, values):
+    def join_by_country_code(self, pays_code, values):
         country_name = None
-        persons = []
-
         for value in values:
             if value[0] == 'country':
                 country_name = value[1]
-            else:
-                persons.append(value[1:])
-
-        for person in persons:
-            prenom, nom = person
-            yield (prenom, nom, country_name), pays_code
+            else:  # 'person' record
+                prenom, nom = value[1:]
+                yield (prenom, nom, country_name), pays_code
 
 if __name__ == '__main__':
     JointurePaysPersonnes.run()
