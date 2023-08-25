0f12a93285eb503cbc09fddbaab5473dfcda44af
Alexander Medina <alexjmedina@users.noreply.github.com>
2023-08-25 12:15:51 -0400

    Update JointurePaysPersonnes.py

--- JointurePaysPersonnes.py
+++ JointurePaysPersonnes.py
@@ -9,18 +9,19 @@ class JointurePaysPersonnes(MRJob):
 
     def mapper_get_pays(self, _, line):
 
-        if "|" in line :
-             colonnes = line.split("|")
-             prenom = colonnes[0]
-             nom = colonnes[1]
-             pays_code = colonnes[2]
-             yield pays_code, prenom_nom
+        colonnes = line.split("|")
+        prenom = colonnes[0]
+        nom = colonnes[1]
+        pays_code = colonnes[2]
+        pays = colonnes[3]
+        prenom_nom_pcode = prenom + ' ' + nom + ' ' + pays_code
+        yield pays, prenom_nom_pcode
                      
 
-    def reducer_compte_pays(self, film_id, film_name_and_eval):
-        film_name_and_eval = [x for x in film_name_and_eval]
-        title = film_name_and_eval[0]
-        for element in film_name_and_eval[1:]:
+    def reducer_compte_pays(self, pays, prenom_nom_pcode):
+        prenom_nom_pcode = [x for x in prenom_nom_pcode]
+        title = prenom_nom_pcode[0]
+        for element in prenom_nom_pcode[1:]:
             yield title, element
 
 if __name__ == '__main__':
