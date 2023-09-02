e02c25d2f2408d9b9e9f822b4d53d0348b53ab0f
Alexander Medina <alexjmedina@users.noreply.github.com>
2023-09-01 20:39:33 -0400

    Updating

---
+++ Eval2/Eval2_Ex3.py
@@ -0,0 +1,20 @@
+""" Evaluation2 - Exercice 3 """
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class CompteNombresPairs(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_nombres_pairs,
+                   reducer=self.reducer_compte_nombres_pairs)       ]
+
+    def mapper_get_nombres_pairs(self, _, line):
+        nombres = int(line)
+        if nombres % 2 == 0:
+            yield nombres, 1
+
+    def reducer_compte_nombres_pairs(self, nombres, npairs):
+        yield nombres, sum(npairs)
+
+if __name__ == '__main__':
+    CompteNombresPairs.run()
