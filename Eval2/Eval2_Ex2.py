0022a3e01fa9645f874ad8471b5888251e7db37e
Alexander Medina <alexjmedina@users.noreply.github.com>
2023-09-08 12:25:23 -0400

    Update Eval2_Ex2.py

--- Eval2/Eval2_Ex2.py
+++ Eval2/Eval2_Ex2.py
@@ -11,10 +11,12 @@ class FiltreNombresPairs(MRJob):
     def mapper_get_nombres_pairs(self, _, line):
         nombres = int(line)
         if nombres % 2 == 0:
-            yield(nombres, None)
+            yield(nombres, 1)
+            #yield(nombres, None)
 
     def reducer_compte_nombres_pairs(self, key, values):
-        yield(key, None)
+        yield(key, sum(values))
+        #yield(key, None)
 
 if __name__ == '__main__':
     FiltreNombresPairs.run()
