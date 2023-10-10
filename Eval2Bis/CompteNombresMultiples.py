d6a42c3e782164f64f3a111f60732402cf6b0b14
Alexander Medina <alexjmedina@users.noreply.github.com>
2023-10-09 23:59:44 -0400

    Creating Eval2 Bis solution

---
+++ Eval2Bis/CompteNombresMultiples.py
@@ -0,0 +1,24 @@
+""" Evaluation2 Bis - Exercice 1 """
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class CompteNombresMultiples(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_nombres_multiples,
+                   reducer=self.reducer_compte_nombres_multiples)
+        ]
+
+    def mapper_get_nombres_multiples(self, _, line):
+        nombres = int(line)
+        if nombres % 3 == 0 and nombres % 5 == 0:
+            yield(None, nombres)
+
+    def reducer_compte_nombres_multiples(self, _, values):
+        multiples = list(values)
+        yield "Nombres multiples de 3 et 5:", multiples
+        yield "Total des nombres multiples de 3 et 5:", sum(multiples)
+        yield "Nombre de nombres multiples de 3 et 5:", len(multiples)
+
+if __name__ == '__main__':
+    CompteNombresMultiples.run()
