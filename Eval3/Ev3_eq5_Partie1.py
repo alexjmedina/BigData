48bb1d5d2c24760444bfe6f8f87e9a57bbf559bc
Alexander Medina <alexjmedina@users.noreply.github.com>
2023-10-11 20:03:36 -0400

    Updating Ev3 Partie1

--- Eval3/Ev3_eq5_Partie1.py
+++ Eval3/Ev3_eq5_Partie1.py
@@ -15,30 +15,35 @@ class NettoyerLesDonnees(MRJob):
         
         colonnes = line.split(' - ')
         livre_title = colonnes[0]
-        auteur = colonnes[1].lower()
-        langue_original = colonnes[2].lower()
-        title_original = colonnes[3].lower()
+        #auteur = colonnes[1].lower()
+        #langue_original = colonnes[2].lower()
+        #title_original = colonnes[3].lower()
+        auteur = colonnes[1]
+        langue_original = colonnes[2]
+        title_original = colonnes[3]
 
-        if "inconnu" in auteur:
+        if "inconnu" in auteur or "Inconnu" in auteur:
             auteur = ""
 
-        if "inconnu" in langue_original:
+        if "inconnu" in langue_original or "Inconnu" in langue_original:
             langue_original = ""
 
-        if "inconnu" in title_original:
+        if "inconnu" in title_original or "Inconnu" in title_original:
             title_original = ""
 
-        if "français" in langue_original:
+        if "français" in langue_original or "Français" in langue_original:
             langue_original = ""
 
-        if "français" in title_original:
+        if "français" in title_original or "Français" in title_original:
             title_original = ""
 
         yield 1, [ livre_title, auteur, langue_original, title_original]
         
     def reducer_get_results(self, _, books):
         book_id = 1
-        with open("C:/Users/Utilisateur/Documents/GitHub/BigData/Eval3/LivresPartie1_eq5.txt",'w',encoding="utf-8") as f:
+        # Nous avons utilisé cette méthode de sortie pour le fichier afin de garantir un encodage correct, 
+        # mais le répertoire de sortie doit être modifié avant d'exécuter le code sur chaque ordinateur
+        with open("C:/Users/6227565/Documents/GitHub/BigData/Eval3/LivresPartie1_eq5.txt",'w',encoding="utf-8") as f:
             for book in books:
                 yield _, bytes( f"{book_id},{','.join(book)}",  'utf-8')
                 f.write(f"{book_id},{','.join(book)}")
