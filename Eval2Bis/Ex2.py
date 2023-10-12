2bbbcd70dbb5c3808571df63b5c3b708fd4b44b8
Alexander Medina <alexjmedina@users.noreply.github.com>
2023-10-12 15:25:39 -0400

    Updating Eval2Bis

---
+++ Eval2Bis/Ex2.py
@@ -0,0 +1,24 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class AuteursPlusQuUnLivre(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_auteurs,
+                   reducer=self.reducer_get_livres)#,
+            #MRStep(reducer=self.reducer_output_books)
+        ]
+
+    def mapper_get_auteurs(self, _, line):
+        data = line.split(',')
+        livre = data[1]
+        auteur = data[2].zfill(3)
+        yield auteur, livre
+
+    def reducer_get_livres(self, auteur, livres):
+        livres_list = list(livres)
+        if len(livres_list) > 1:
+            yield auteur.lstrip('0'), livres_list
+
+if __name__ == '__main__':
+    AuteursPlusQuUnLivre.run()
